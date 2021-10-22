bbaa8e78f215a9b41a58e9a29d1acd8e51ae3ca3
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-10-22 14:24:05 -0700

    Add provisioner image

---
+++ provisioner/image/provisioner_main.py
@@ -0,0 +1,14 @@
+#
+# provisioner startup python script
+#
+# BSD license, copyright Igor Sfiligoi@UCSD 2021
+#
+# Requires two arguments:
+#   Namespce
+#   CVMFS_Mounts (comma separated list)
+#
+
+import sys
+import t2_provisioner.provisioner
+
+t2_provisioner.provisioner.main(sys.argv[1], sys.argv[2].split(","))
---
+++ provisioner/image/provisioner_startup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# basic setup
+/opt/provisioner/setup_k8s_creds.sh
+
+if [ "x${K8S_NAMESPACE}" == "x" ]; then
+  echo "Missing K8S_NAMESPACE"
+  sleep 15
+  exit 1
+fi
+
+if [ "x${CVMFS_MOUNTS}" == "x" ]; then
+  echo "Missing CVMFS_MOUNTS"
+  sleep 15
+  exit 1
+fi
+
+trap 'echo signal received!; kill $(jobs -p); wait' SIGINT SIGTERM
+
+echo "`date` Starting provisioner_main.py"
+su provisioner -c "cd /home/provisioner && python3 provisioner_main.py ${K8S_NAMESPACE} ${CVMFS_MOUNTS}" &
+wait
+rc=$?
+echo "`date` End of provisioner_main.py, rc=${rc}"
+
