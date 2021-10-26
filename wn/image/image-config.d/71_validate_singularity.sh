02a89f5aa631b5ae486601c37a0248e2f788ee69
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-10-26 15:52:33 -0700

    Add unprivileged test

--- wn/image/image-config.d/71_validate_singularity.sh
+++ wn/image/image-config.d/71_validate_singularity.sh
@@ -11,4 +11,14 @@ if [ -f "/usr/bin/singularity" ]; then
     echo "Singularity test execution failed!"
     exit 1
   fi
+
+  # this is the real test, as used by HTCondor
+  su htcuser -c '/usr/bin/singularity exec --contain --ipc --pid --bind /cvmfs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el7:latest /usr/bin/dc -e "3 5 + p"'
+  rc=$?
+
+  if [ $rc -ne 0 ]; then
+    echo "Unprivileged Singularity test execution failed!"
+    exit 1
+  fi
+
 fi
